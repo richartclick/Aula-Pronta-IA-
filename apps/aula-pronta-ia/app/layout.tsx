f515c0c4a11d23c84597da034f8c2699833bb6b5
richartclick <richartclick@gmail.com>
2026-06-02 19:52:30 -0300

    feat: instalar Pinterest Tag pixel 2614036570742

--- apps/aula-pronta-ia/app/layout.tsx
+++ apps/aula-pronta-ia/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght,SOFT@0,9..144,300..900,30..100;1,9..144,300..900,30..100&family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        {/* Pinterest Tag */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(e){if(!window.pintrk){window.pintrk=function(){window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+          pintrk('load', '2614036570742', {em: '<user_email_address>'});
+          pintrk('page');
+        ` }} />
+        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none;" alt="" src="https://ct.pinterest.com/v3/?event=init&tid=2614036570742&pd[em]=<user_email_address>&noscript=1" />` }} />
       </head>
       <body className="min-h-full">{children}</body>
     </html>
